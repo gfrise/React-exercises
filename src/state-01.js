6cf62cd25fb9897e23f18ad9eae7c9a2f3ae2cfe
gfrise <104461249+gfrise@users.noreply.github.com>
2022-09-02 15:07:51 +0200

    state-01-ex

---
+++ src/state-01.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Counter extends React.Component {
+    state = {
+        count: 0
+    }
+
+    constructor(props) {
+        super(props);
+        
+        setInterval(() => {
+            this.setState((state) => {
+                return {
+                    count: state.count +1,
+                }
+            })
+        }, 1000)
+    } 
+
+    render() {
+        return <h1>{this.state.count}</h1>
+    }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Counter />);
