691a4f3b79c8cbeef38a876952a392d09f58646c
gfrise <104461249+gfrise@users.noreply.github.com>
2022-09-06 12:53:27 +0200

    conditional-rendering-01-ex

---
+++ src/Welcome.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Age } from './Age';
+
+export const name = <strong>Giuseppe</strong>
+
+export class Welcome extends React.Component {
+    render() {
+        return (
+            <>
+                <p>Welcome {this.props.name}</p>
+                {this.props.age >= 18 && <Age age={this.props.age}/>}
+            </>
+        )
+    }
+}
