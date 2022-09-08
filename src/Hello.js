97c2d8756ccafbe57fc339741928d338395c1722
gfrise <104461249+gfrise@users.noreply.github.com>
2022-09-08 16:08:55 +0200

    conditional-rendering-02

---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Message } from './Message';
+
+export class Hello extends React.Component {
+    render() {
+        return (
+            <>
+                <h1>Hello World</h1>
+                < Message />
+            </>
+        )
+    }
+}
