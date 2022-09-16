8291cb63fc7f9894763de2624bc751dbe9b4461b
gfrise <104461249+gfrise@users.noreply.github.com>
2022-09-16 09:57:49 +0200

    forms-05

---
+++ src/Login.js
@@ -0,0 +1,39 @@
+import React, { createRef } from 'react';
+
+export class UncontrolledLogin extends React.Component {
+    _ref = createRef();
+
+    state = {
+        disabled: false,
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        const name = event.target.elements.username.value;
+        const password = event.target.elements.password.value;
+        const remember = event.target.elements.checkbox.value;
+        this.props.onLogin({name, password, remember})
+    }
+
+    handle = () => {
+        const name = this._formRef.current.elements.username.value
+        const password = this._formRef.current.elements.password.value
+        this.setState({
+            disabled: !!username && !!password,
+        })
+    }
+
+    render() {
+        return (
+            <>
+                <form ref={this._ref} onSubmit={this.handleSubmit}>
+                    <input name="username" onChange={this.handle} />
+                    <input name="password" type="password" onChange={this.handle} />
+                    <input name="checkbox" type="checkbox"  />
+                    <input name="submit" type="submit" disabled={!this.state.disabled}>Login</input>
+                    <input name="reset" type="reset" onClick={this.handleReset}/>
+                </form>
+            </>
+        )
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { Login } from './Login';
+import ReactDOM from 'react-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+render.root(<UncontrolledLogin />);
