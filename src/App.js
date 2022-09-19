ea4f24a96d141a96bb645722aad37ce8a9d2b224
gfrise <104461249+gfrise@users.noreply.github.com>
2022-09-19 21:16:48 +0200

    context-01

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { createContext } from 'react'
+
+export const LanguageContext = createContext('en')
+
+export class DisplayLanguage extends React.Component {
+    render() {
+        return <></>
+    }
+}
+
+export class App extends React.Component {
+    state = {
+        lingua: 'en',
+    }
+    handle = event => {
+        this.setState({
+            language: event.target.language.value,
+        })
+    }
+    render () {
+        return (
+            <>
+                <select name="lingua" value={this.state.lingua} onChange={this.handle}>
+                    <option value='en'>English</option>
+                    <option value='it'>Italiano</option>
+                </select>
+                <LanguageContext.Provider value={this.state.lingua}>
+                    <DisplayLanguage />
+                </LanguageContext.Provider>
+            </>
+        )
+    }
+}
