6a3c2cf50589f2ae1a490d97cbeafac6ac11888c
gfrise <104461249+gfrise@users.noreply.github.com>
2022-09-21 12:44:32 +0200

    usestate-01

---
+++ src/ClickCounter.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+
+export function ClickCounter({ onCounterChange }) {
+    const [count, setCount] = useState(0)
+
+    useEffect(()=> {
+        onCounterChange(count)
+    }, [count, onCounterChange])
+
+	const handleIncrement = () => {
+		setCount((count) => count + 1)
+		}
+    return (
+        <>
+            <h1>{count}</h1>
+            <button onClick={handleIncrement}>Incrementa</button>
+        </>
+    ) 
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { ClickCounter } from './ClickCounter.js';
+import ReactDOM from 'react-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<ClickCounter onChange={onCounterChange} />);
