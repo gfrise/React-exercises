5eb714ef5ffd3a35521c3e431c1a3ceeaec37412
gfrise <104461249+gfrise@users.noreply.github.com>
2022-09-22 20:52:45 +0200

    form6

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { UncontrolledLogin } from './UncontrolledLogin';
+import ReactDOM from 'react-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+render.root(<UncontrolledLogin />);
