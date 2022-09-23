2da42d7c48441e47a31b698c0dfdd0c24dbf08ea
gfrise <104461249+gfrise@users.noreply.github.com>
2022-09-23 17:18:36 +0200

    router5

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ export function App() {
 			<Route path="/" element={<Welcome name="Jim" />} />
             <Route path="/counter" element={<ClickCounter />} />
             <Route path="/:username" element={<ShowGithubUser />} />
+			<Route path="*" element={<h1>Not Found</h1>} />
 		</Routes>
 		</>
 	)
