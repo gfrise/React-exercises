bacff163b6fa9a7d88756aafbe672e2b442332c7
gfrise <104461249+gfrise@users.noreply.github.com>
2022-09-23 17:40:29 +0200

    router6

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Route, Routes, useParams, Link } from 'react-router-dom'
 import { Welcome } from './Welcome'
 import { GithubUser } from './GithubUser'
+import { GithubUserList } from './GithubUserList'
 import { ClickCounter } from './ClickCounter'
 
 export function ShowGithubUser() {
@@ -12,12 +13,14 @@ export function App() {
 	return (
 		<>
 		 <h1>Links:</h1>
-        <p><Link to={"/"}>Home</Link> | <Link to={"counter"}>Counter</Link> | <Link to={"/gfrise"}>Github</Link></p>
+        <Link to={"/"}>Home</Link> | <Link to={"counter"}>Counter</Link> | <Link to={"/gfrise"}>Github</Link>
 		<Routes>
-			<Route path="/" element={<Welcome name="Jim" />} />
-            <Route path="/counter" element={<ClickCounter />} />
-            <Route path="/:username" element={<ShowGithubUser />} />
-			<Route path="*" element={<h1>No path matched</h1>} />
+			<Route path='/' element={<Welcome name="Jim" />} />
+            <Route path='/counter' element={<ClickCounter />} />
+            <Route path="/users" element={<GithubUserList />}>
+				<Route path=":username" element={<ShowGithubUser />} />
+			</Route>
+            <Route path="*" element={<h1>No path matched</h1>} />
 		</Routes>
 		</>
 	)
