eff9fa684ff8dd768101d9f3a568f73b134e0c09
gfrise <104461249+gfrise@users.noreply.github.com>
2022-09-25 19:24:59 +0200

    swr-03

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -2,13 +2,20 @@ import React, { useEffect, useState } from 'react';
 import { useGithubUser } from './useGithubUser';
 
 export function GithubUser({username}) {
-    const {data, error} = useGithubUser(username);
+    const {data, error, mutate} = useGithubUser(username);
+
+    
+    function onRefresh() {
+        mutate()
+    }
+
 
     return (
         <>
             {data && <p>Il nome è {data.name}</p>}
             {data && <p>Questo è il link: {data.url}</p>}
             {error && <p>Err 404</p>}
+            <button onClick={onRefresh}>Refresh</button>
         </>
     )
 }
--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -3,7 +3,7 @@ import useSWR from 'swr'
 const fetcher = (url) => fetch(url).then((r) => r.json())
 
 export function useGithubUser(username) {
-	const { data, error } = useSWR(username ? `https://api.github.com/users/${username}` : null, fetcher)
-    
-	return {data, error}
+	const { data, error, mutate } = useSWR(username ? `https://api.github.com/users/${username}` : null, fetcher)
+
+	return {data, error, mutate}
 }
